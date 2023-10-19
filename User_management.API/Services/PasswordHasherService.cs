638c7d88670001079f7c88b480d10ef34bfcb6e7
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-19 17:17:07 +0200

    Adding PasswordHasherService

---
+++ User_management.API/Services/PasswordHasherService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace User_management.API.Services
+{
+    public static class PasswordHasherService
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashValue = SHA256.HashData(messageBytes);
+            return Convert.ToHexString(hashValue);
+        }
+    }
+}
