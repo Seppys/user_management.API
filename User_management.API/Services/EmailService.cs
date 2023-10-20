50c6d02361a95a606c1b89e6b3adb7adc50ad3ea
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-21 01:51:53 +0200

    Adding EmailService

---
+++ User_management.API/Services/EmailService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace User_management.API.Services
+{
+    public class EmailService
+    {
+        public static async void SendEmail(string email, string subject, string body)
+        {
+            string emailSenderApiUrl = "https://notificatorapi.azurewebsites.net/api/email/send";
+
+            var data = new
+            {
+                address = email,
+                subject = subject,
+                body = body
+            };
+
+            using (HttpClient client = new HttpClient()) 
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(data), 
+                    System.Text.Encoding.UTF8, "application/json");
+
+                client.PostAsync(emailSenderApiUrl, content);
+            }
+        }
+    }
+}
