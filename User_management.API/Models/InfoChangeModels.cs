93ba12a8678de702c853b4bc052e5b6bbd746422
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-21 00:36:18 +0200

    Modifying InfoChangeModels

--- User_management.API/Models/InfoChangeModels.cs
+++ User_management.API/Models/InfoChangeModels.cs
@@ -20,19 +20,11 @@ namespace User_management.API.Models
             [Required]
             [RegularExpression("[a-zA-Z0-9_]+")]
             [StringLength(20, MinimumLength = 5)]
-            public string CurrentUsername { get; set; }
-
-            [Required]
-            [RegularExpression("[a-zA-Z0-9_]+")]
-            [StringLength(20, MinimumLength = 5)]
             public string NewUsername { get; set; }
         }
 
         public class ChangeEmailModel
         {
-            [Required]
-            [EmailAddress]
-            public string CurrentEmail { get; set; }
 
             [Required]
             [EmailAddress]
