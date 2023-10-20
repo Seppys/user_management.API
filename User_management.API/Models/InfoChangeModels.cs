845d4af5dbb16a6478dc9cededd7d011cbf1c664
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-21 00:28:15 +0200

    Adding InfoChangeModels

---
+++ User_management.API/Models/InfoChangeModels.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User_management.API.Models
+{
+    public class InfoChangeModels
+    {
+        public class ChangePasswordModel
+        {
+            [Required]
+            [MinLength(5)]
+            public string CurrentPassword { get; set; }
+
+            [Required]
+            [MinLength(5)]
+            public string NewPassword { get; set; }
+        }
+
+        public class ChangeUsernameModel
+        {
+            [Required]
+            [RegularExpression("[a-zA-Z0-9_]+")]
+            [StringLength(20, MinimumLength = 5)]
+            public string CurrentUsername { get; set; }
+
+            [Required]
+            [RegularExpression("[a-zA-Z0-9_]+")]
+            [StringLength(20, MinimumLength = 5)]
+            public string NewUsername { get; set; }
+        }
+
+        public class ChangeEmailModel
+        {
+            [Required]
+            [EmailAddress]
+            public string CurrentEmail { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string NewEmail { get; set; }
+        }
+    }
+}
