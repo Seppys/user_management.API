85ea7953c71f81ceab847907a78fb62f2c1ceb08
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-23 09:53:04 +0200

    Adding password recovery model

---
+++ User_management.API/Models/PasswordRecoveryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User_management.API.Models
+{
+    public class PasswordRecoveryModel
+    {
+        [Required]
+        [RegularExpression("[a-zA-Z0-9_]+")]
+        [StringLength(20, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
